bbdf30076f8ea4deac17c87fcd04fe591519f3db
Girish <girishd22@gmail.com>
2019-08-16 22:26:04 -0500

    Added for loop

--- src/main/java/jenkinsexample/HelloWorld/App.java
+++ src/main/java/jenkinsexample/HelloWorld/App.java
@@ -8,7 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("From Girish");
+        for(int i=0;i<10;i++)
+        {
+        	System.out.println( "Hello World!" );
+        }
     }
 }
